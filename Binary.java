9612e9630022914a90a5aa015723b8085a959712
Leonid <33938416+Leonid1999@users.noreply.github.com>
2018-04-03 23:37:00 +0300

    Update Binary.java

--- Binary.java
+++ Binary.java
@@ -9,7 +9,7 @@ package javaapplication1;
  *
  * @author lenya
  */
-public class Binary {
+public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = { 1,2,3,4,5,6,7,8,9,10 };
         System.out.println(rank(1, arr));
