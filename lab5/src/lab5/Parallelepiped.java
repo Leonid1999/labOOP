9f15bb7a315e494358b8cac2ac6e9bccc0626eac
Leonid <33938416+Leonid1999@users.noreply.github.com>
2018-04-17 22:01:37 +0300

    Update Parallelepiped.java

--- lab5/src/lab5/Parallelepiped.java
+++ lab5/src/lab5/Parallelepiped.java
@@ -46,16 +46,16 @@ public class Parallelepiped {
         this.c = c;
     }
     
-    public double Area(){
+    public double area(){
     double s=2*(a*b+a*c+b*c);
     return s;
     }
     
-    public double Voluem(){
+    public double Volume(){
       double v=a*b*c; 
      return v;
     }
-      public void Compration(){
+      public void compration(){
        double v=a*b*c;
        double s=2*(a*b+a*c+b*c);
         if(v<s){
